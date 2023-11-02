247712285a07c670e5b0c2c1ddb5cc21dcc9ae2d
Neelam Verma <neelam.vma11@gmail.com>
2023-11-03 00:47:54 +0530

    appjs page clear data

--- src/App.js
+++ src/App.js
@@ -3,19 +3,14 @@ import MainPage from './Components/MainPage';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './Components/Navbar';
 import FooterPage from './Components/FooterPage';
-import AboutPage from './Components/AboutPage';
-import Featurs from './Components/Featurs';
 import GuidePage from './Components/GuidePage';
 
 function App() {
   return (
     <BrowserRouter basename='/go-wild-website'>
       <Navbar />
-      {/* <MainPage /> */}
       <Routes>
         <Route path='/' element={<MainPage />} />
-        {/* <Route path='/featur' element={<Featurs />} />
-        <Route path='/about' element={<AboutPage />} /> */}
         <Route path='/guide' element={<GuidePage />} />
       </Routes>
       <FooterPage />
